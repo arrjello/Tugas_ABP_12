c306b074d0bc302e2e611c99142a03dbc1483dbd
Ajay <arrijaljulfa29@gmail.com>
2026-06-03 20:46:02 +0700

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'tutorial_11-1.dart';
+import 'tutorial_12.dart';
+import 'tutorial_13.dart';
+
+void main() => runApp(MaterialApp(
+  title: 'Geolocation',
+  theme: ThemeData(
+    primarySwatch: Colors.blue,
+  ),
+  debugShowCheckedModeBanner: false,
+  home: const MyApp13(),
+));
+
